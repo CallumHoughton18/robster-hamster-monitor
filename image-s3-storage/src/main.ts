712e160311abd6fe1e6e564a957d18ed4de2d514
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2023-06-19 00:58:38 +0100

    add posting snapshot videos to S3

--- image-s3-storage/src/main.ts
+++ image-s3-storage/src/main.ts
@@ -8,8 +8,10 @@ const logger = pinoLogger("S3-image-storage");
 
 const config = {
   redisUrl: process.env.REDIS_URL as string,
-  redisChannelName: process.env.REDIS_IMAGES_CHANNEL as string,
-  bucketName: process.env.BUCKET_NAME as string,
+  redisImagesChannelName: process.env.REDIS_IMAGES_CHANNEL as string,
+  redisImagesVideosName: process.env.REDIS_VIDEOS_CHANNEL as string,
+  imagesBucketName: process.env.IMAGES_BUCKET_NAME as string,
+  videosBucketName: process.env.VIDEOS_BUCKET_NAME as string,
   s3Region: process.env.S3_REGION as string,
 };
 
@@ -20,12 +22,12 @@ const imageUploader = async () => {
   const subscriber = redisClient.duplicate();
 
   await subscriber.connect();
-  logger.info(`Connected to Redis Channel ${config.redisChannelName}`);
+  logger.info(`Connected to Redis`);
 
   await subscriber.subscribe(
-    config.redisChannelName,
+    config.redisImagesChannelName,
     buf => {
-      logger.info(`Detected bytes on channel ${config.redisChannelName}`);
+      logger.info(`Detected bytes on channel ${config.redisImagesChannelName}`);
       const now = new Date(); // Get the current local date and time
       const utcDate = new Date(
         Date.UTC(
@@ -38,14 +40,35 @@ const imageUploader = async () => {
           now.getUTCMilliseconds()
         )
       );
-      publishImageDataToS3(utcDate, buf);
+      publishImageDataToS3(config.imagesBucketName, ".webp", utcDate, buf);
+    },
+    true
+  );
+
+  await subscriber.subscribe(
+    config.redisImagesVideosName,
+    buf => {
+      logger.info(`Detected bytes on channel ${config.redisImagesVideosName}`);
+      const now = new Date(); // Get the current local date and time
+      const utcDate = new Date(
+        Date.UTC(
+          now.getUTCFullYear(),
+          now.getUTCMonth(),
+          now.getUTCDate(),
+          now.getUTCHours(),
+          now.getUTCMinutes(),
+          now.getUTCSeconds(),
+          now.getUTCMilliseconds()
+        )
+      );
+      publishImageDataToS3(config.videosBucketName, ".mp4", utcDate, buf);
     },
     true
   );
 
   logger.info(`Initialized S3 image uploader`);
 
-  async function publishImageDataToS3(publishDateTime: Date, buf: Buffer) {
+  async function publishImageDataToS3(bucketName: string, fileExtension: string, publishDateTime: Date, buf: Buffer) {
     const year = publishDateTime.getFullYear();
     const month = String(publishDateTime.getMonth() + 1).padStart(2, "0"); // Months are zero-based
 
@@ -54,9 +77,9 @@ const imageUploader = async () => {
     const minutes = String(publishDateTime.getMinutes()).padStart(2, "0");
     const seconds = String(publishDateTime.getSeconds()).padStart(2, "0");
 
-    const key = `${year}/${month}/${day}/${hours}:${minutes}:${seconds}.webp`;
+    const key = `${year}/${month}/${day}/${hours}:${minutes}:${seconds}${fileExtension}`;
     const command = new PutObjectCommand({
-      Bucket: config.bucketName,
+      Bucket: bucketName,
       Key: key,
       Body: buf,
     });
